0250f4d6c414e4375bbb696224224eead61bdb43
omayconmendes <mayconsouzamendes@gmail.com>
2021-10-18 21:32:12 -0300

    Update delete components
    Update student-delete.component;
    Update class-delete.component;
    Create header.model.ts;
    Create headerData.service.ts;
    *html;
    *css;
    *typescript;

--- frontend/src/app/services/crud.service.ts
+++ frontend/src/app/services/crud.service.ts
@@ -1,4 +1,5 @@
-import { Observable } from 'rxjs';
+import { EMPTY, Observable } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
 
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -16,62 +17,98 @@ export class CrudService {
 
   constructor( private snackbar: MatSnackBar, private http: HttpClient ) { }
 
-  showMessage(msg: string): void {
+  showMessage(msg: string , isError: boolean = false): void {
     this.snackbar.open(msg, 'X', {
       duration: 3000,
       horizontalPosition: 'center',
-      verticalPosition: 'bottom'
-    })
+      verticalPosition: 'bottom',
+      panelClass: isError? ['msg-error'] : ['msg-success']
+    });
+  }
+
+  errorHandler(e: any): Observable<any> {
+    this.showMessage('Ocorreu um erro.' , true);
+    return EMPTY;
   }
 
   // ============================== Student Methods ============================== //
 
   createStudent(student: Student): Observable<Student> {
-    return this.http.post<Student>(this.studentUrl, student)
+    return this.http.post<Student>(this.studentUrl, student).pipe(
+      map((obj) => obj),
+      catchError((e) => this.errorHandler(e))
+    );
   };
 
   readStudent(): Observable<Student[]> {
-    return this.http.get<Student[]>(this.studentUrl)
+    return this.http.get<Student[]>(this.studentUrl).pipe(
+      map((obj) => obj),
+      catchError((e) => this.errorHandler(e))
+    );
   }
 
   readStudentById(id: String): Observable<Student> {
     const url = `${this.studentUrl}/${id}`
-    return this.http.get<Student>(url);
+    return this.http.get<Student>(url).pipe(
+      map((obj) => obj),
+      catchError((e) => this.errorHandler(e))
+    );
   }
 
   updateStudent(student: Student): Observable<Student> {
     const url = `${this.studentUrl}/${student.id}`
-    return this.http.put<Student>(url , student);
+    return this.http.put<Student>(url , student).pipe(
+      map((obj) => obj),
+      catchError((e) => this.errorHandler(e))
+    );
   }
 
-  deleteStudent(id: string): Observable<Student> {
-    const url = `${this.studentUrl}/${id}`;
-    return this.http.delete<Student>(url);
+  deleteStudent(student: Student): Observable<Student> {
+    const url = `${this.studentUrl}/${student.id}`;
+    return this.http.delete<Student>(url).pipe(
+      map((obj) => obj),
+      catchError((e) => this.errorHandler(e))
+    );
   }
 
   // ============================== Class Methods ============================== //
 
   createClass(classes: Class): Observable<Class> {
-    return this.http.post<Class>(this.classUrl, classes)
-  }
+    return this.http.post<Class>(this.classUrl, classes).pipe(
+      map((obj) => obj),
+      catchError((e) => this.errorHandler(e))
+    );
+  };
 
   readClass(): Observable<Class[]> {
-    return this.http.get<Class[]>(this.classUrl)
+    return this.http.get<Class[]>(this.classUrl).pipe(
+      map((obj) => obj),
+      catchError((e) => this.errorHandler(e))
+    );
   }
   
   readClassById(id: String): Observable<Class> {
     const url = `${this.classUrl}/${id}`
-    return this.http.get<Class>(url);
+    return this.http.get<Class>(url).pipe(
+      map((obj) => obj),
+      catchError((e) => this.errorHandler(e))
+    );
   }
 
   updateClass(classes: Class): Observable<Class> {
     const url = `${this.classUrl}/${classes.id}`
-    return this.http.put<Class>(url , classes);
+    return this.http.put<Class>(url , classes).pipe(
+      map((obj) => obj),
+      catchError((e) => this.errorHandler(e))
+    );
   }
 
   deleteClass(classes: Class): Observable<Class> {
     const url = `${this.classUrl}/${classes.id}`;
-    return this.http.delete<Class>(url);
+    return this.http.delete<Class>(url).pipe(
+      map((obj) => obj),
+      catchError((e) => this.errorHandler(e))
+    );
   }
 
 }
--- frontend/src/app/views/student-crud/student-delete/student-delete.component.ts
+++ frontend/src/app/views/student-crud/student-delete/student-delete.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Class } from 'src/app/Models/class.model';
+import { Student } from 'src/app/Models/student.model';
+import { CrudService } from 'src/app/services/crud.service';
 
 @Component({
   selector: 'app-student-delete',
   templateUrl: './student-delete.component.html',
   styleUrls: ['./student-delete.component.css']
 })
+
 export class StudentDeleteComponent implements OnInit {
 
-  constructor() { }
+  student!: Student;
+  class!: Class;
+
+  constructor(private router: Router,
+              private crudService: CrudService,
+              private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id') as string;
+    this.crudService.readStudentById(id).subscribe(student => {
+      this.student = student;
+    })
+  }
+
+  deleteStudent(): void {
+    this.crudService.deleteStudent(this.student).subscribe( () => {
+      this.crudService.showMessage('Aluno excluído com sucesso !');
+      this.router.navigate(['/students']);
+    })
+  }
+
+  cancel(): void {
+    this.router.navigate(['/students']);
   }
 
 }
