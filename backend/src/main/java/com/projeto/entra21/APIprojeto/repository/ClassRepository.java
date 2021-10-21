8336328d35637156fb397ab520cdb660596b0b21
Welinton Oliveira <you@example.com>
2021-10-20 23:55:20 -0300

    Create repository class

---
+++ backend/src/main/java/com/projeto/entra21/APIprojeto/repository/ClassRepository.java
@@ -0,0 +1,7 @@
+package com.projeto.entra21.APIprojeto.repository;
+
+import com.projeto.entra21.APIprojeto.entity.Class;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClassRepository extends JpaRepository<Class, Integer> {
+}
